4ef6587dabdec25982d9f364fc4d0ac9189ff691
tkorays <tkorays@hotmail.com>
2016-04-10 11:22:25 +0800

    implement cmd framework

--- v2/codexml.h
+++ v2/codexml.h
@@ -1,4 +1,40 @@
 #ifndef __CODEXML_H__
 #define __CODEXML_H__
 
+
+#define CX_CODE_DESC_SIZE 128
+#define CX_CMD_NAME_SIZE 32
+
+#define CX_NULL (void*)0
+
+typedef int CX_RET;
+typedef void* CX_NodePtr;
+typedef CX_RET (*CX_CMD_FUNC)(CX_NodePtr node);
+
+typedef enum {
+    CX_SUCCESS=0,
+    CX_FAILED,
+    CX_MEMERR,
+
+    CX_CODE_NUM
+} CX_CODE;
+
+typedef struct {
+    int code;
+    char desc[CX_CODE_DESC_SIZE];
+} CX_CodeInfo;
+
+typedef struct {
+    char        name[CX_CMD_NAME_SIZE];
+    CX_CMD_FUNC func;
+} CX_CmdInfo;
+
+extern CX_CodeInfo g_CX_CodeInfos[CX_CODE_NUM];
+#define CX_GetCodeInfo(code) g_CX_CodeInfos[(code)]
+
+CX_RET CX_Proc(CX_NodePtr node);
+CX_RET CX_InstallCmds();
+CX_RET CX_UninstallCmds();
+CX_CMD_FUNC CX_GetCmdFunc(char* name);
+
 #endif // __CODEXML_H__
---
+++ v2/codexml_cmd.c
@@ -0,0 +1,76 @@
+#include "codexml.h"
+#include "mem_pool.h
+#include "mem_pool.h"
+
+#include <string.h>
+
+// 调用者不关心命令是以什么存储的,这里只要提供注册,查询
+
+typedef struct _CmdInfo {
+    CX_CmdInfo          cmd;
+    struct _CodeInfo*   next;
+} CmdInfo;
+
+typedef struct {
+    CmdInfo* first;
+    CmdInfo* current;
+} CmdInfoList;
+
+CmdInfoList g_CmdList;
+mem_pool_t* g_mpool;
+
+// 定义基本函数,对外不可见
+CX_RET cmd_load(CX_NodePtr node);
+CX_RET cmd_unload(CX_NodePtr node);
+CX_RET cmd_call(CX_NodePtr node);
+
+CmdInfo* CreateCmd(char* name, CX_CMD_FUNC func);
+void InsertCmd(CmdInfoList* list, char* name, CX_CMD_FUNC func);
+
+CX_RET CX_InstallCmds(){
+    CmdInfoList* cmd_list = &g_CmdList;
+    g_mpool = mem_pool_create();
+    if(!g_mpool){
+        return CX_MEMERR;
+    }
+
+    InsertCmd(cmd_list, "proc", CX_Proc);
+    InsertCmd(cmd_list, "load", cmd_load);
+    InsertCmd(cmd_list, "unload", cmd_unload);
+    InsertCmd(cmd_list, "call", cmd_call);
+
+    return CX_SUCCESS;
+}
+
+CX_CMD_FUNC CX_GetCmdFunc(char* name){
+    CmdInfo* cmd = g_CmdList.first;
+    while(cmd){
+        if(strcmp(cmd->cmd.name,name)==0){
+            return cmd->cmd.func;
+        }
+        cmd = cmd->next;
+    }
+    return CX_NULL;
+}
+
+
+CmdInfo* CreateCmd(char* name, CX_CMD_FUNC func){
+    CmdInfo* cmd = (CmdInfo*)mem_pool_alloc(g_mpool,sizeof(CmdInfo));
+    if(!cmd){
+        return CX_NULL;
+    }
+
+    cmd->next = 0;
+    memcpy(cmd->cmd.name,name,strlen(name)+1);
+    cmd->cmd.func = func;
+    return cmd;
+}
+void InsertCmd(CmdInfoList* list, char* name, CX_CMD_FUNC func){
+    if(!list || !name || !func){
+        return ;
+    }
+    list->current->next = CreateCmd(name,func);
+    list->current = list->current->next;
+}
+
+
